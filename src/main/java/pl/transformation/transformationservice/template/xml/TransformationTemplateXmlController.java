127716cb01cbf817d0a3d8111004981633733345
Konrad Dawydiuk <konrad.dawydiuk@gmail.com>
2023-06-19 15:28:04 +0200

    update swagger

--- src/main/java/pl/transformation/transformationservice/template/xml/TransformationTemplateXmlController.java
+++ src/main/java/pl/transformation/transformationservice/template/xml/TransformationTemplateXmlController.java
@@ -1,7 +1,12 @@
 package pl.transformation.transformationservice.template.xml;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,23 +26,57 @@ public class TransformationTemplateXmlController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
-    @Operation(summary = "Create a new template")
-    public ResponseEntity<XSLTTemplateXml> createTemplate(@RequestBody String xsltTemplate) {
+    @Operation(
+            summary = "Create a new template",
+            description = "Example XML: <xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n" +
+                    "    <xsl:output method=\"xml\" indent=\"yes\"/>\n" +
+                    "    <xsl:param name=\"infoParam\" select=\"'\\''additional information saved'\\''\" />\n" +
+                    "    <xsl:template match=\"/\">\n" +
+                    "        <xsl:copy>\n" +
+                    "            <xsl:apply-templates select=\"*\"/>\n" +
+                    "        </xsl:copy>\n" +
+                    "    </xsl:template>\n" +
+                    "    <xsl:template match=\"*\">\n" +
+                    "        <xsl:copy>\n" +
+                    "            <xsl:copy-of select=\"@*\"/>\n" +
+                    "            <xsl:apply-templates select=\"node()\"/>\n" +
+                    "        </xsl:copy>\n" +
+                    "    </xsl:template>\n" +
+                    "    <xsl:template match=\"text()\">\n" +
+                    "        <xsl:if test=\"normalize-space(.) != '\\'''\\''\">\n" +
+                    "            <xsl:value-of select=\"normalize-space(.)\"/>\n" +
+                    "        </xsl:if>\n" +
+                    "    </xsl:template>\n" +
+                    "    <xsl:template match=\"person\">\n" +
+                    "        <xsl:copy>\n" +
+                    "            <xsl:apply-templates select=\"@*|node()\"/>\n" +
+                    "            <info><xsl:value-of select=\"normalize-space($infoParam)\"/></info>\n" +
+                    "        </xsl:copy>\n" +
+                    "    </xsl:template>\n" +
+                    "</xsl:stylesheet>"
+    )
+    public ResponseEntity<XSLTTemplateXml> createTemplate(@org.springframework.web.bind.annotation.RequestBody String xsltTemplate) {
         return new ResponseEntity<>(transformationTemplateXmlService.createTemplate(xsltTemplate), HttpStatus.CREATED);
     }
 
     @GetMapping()
+    @Operation(summary = "Get all templates")
     public ResponseEntity<List<XSLTTemplateXml>> getAllTemplates() {
         return new ResponseEntity<>(transformationTemplateXmlService.getAllTemplates(),
                 HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<XSLTTemplateXml> getTemplateById(@PathVariable("id") String id) {
+    @Operation(summary = "Get template by ID")
+    public ResponseEntity<XSLTTemplateXml> getTemplateById(
+            @PathVariable("id")
+            @Parameter(description = "ID of the template to be obtained.", required = true)
+            String id
+    ) {
         XSLTTemplateXml templateById = transformationTemplateXmlService.getTemplateById(id);
         return Objects.isNull(templateById.getId()) ?
-             new ResponseEntity<>(templateById, HttpStatus.NO_CONTENT) :
-        new ResponseEntity<>(templateById, HttpStatus.OK);
+                new ResponseEntity<>(templateById, HttpStatus.NO_CONTENT) :
+                new ResponseEntity<>(templateById, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}")
